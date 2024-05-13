603a071c3c0ffeff20de41605cd5dd69d700d235
GeorgeSalu <george.salu10@gmail.com>
2024-05-13 11:42:26 -0300

    introducao ao spring boot - projeto vendas - container ioc do spring framework

---
+++ vendas/src/main/java/com/example/vendas/repository/MeuRepository.java
@@ -0,0 +1,10 @@
+package com.example.vendas.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MeuRepository {
+
+	
+	
+}
---
+++ vendas/src/main/java/com/example/vendas/service/MeuServico.java
@@ -0,0 +1,8 @@
+package com.example.vendas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeuServico {
+
+}
