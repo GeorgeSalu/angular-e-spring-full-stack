ca9e3fdc2ca68719bab022bb4df728a80b3a5cc3
GeorgeSalu <george.salu10@gmail.com>
2024-05-14 18:39:11 -0300

    introducao ao spring boot - projeto vendas - criando annotation customizada de configuracao

--- vendas/src/main/java/com/example/vendas/MinhaConfiguration2.java
+++ vendas/src/main/java/com/example/vendas/MinhaConfiguration2.java
@@ -2,11 +2,8 @@ package com.example.vendas;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MinhaConfiguration2 {
 
 	@Bean
