dfba370f9318b807be1f3b5f9486c8167eb8cbc7
GeorgeSalu <george.salu10@gmail.com>
2024-05-16 19:30:53 -0300

    projeto cliente - remove teste incial de cadastro de cliente

--- clientes/src/main/java/com/example/clientes/ClientesApplication.java
+++ clientes/src/main/java/com/example/clientes/ClientesApplication.java
@@ -1,24 +1,10 @@
 package com.example.clientes;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.clientes.model.entity.Cliente;
-import com.example.clientes.model.repository.ClienteRepository;
 
 @SpringBootApplication
 public class ClientesApplication {
-	
-	@Bean
-	public CommandLineRunner run(@Autowired ClienteRepository repository) {
-		return args -> {
-			Cliente cliente = Cliente.builder().cpf("00000000000").nome("fulano").build();
-			repository.save(cliente);
-		};
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
