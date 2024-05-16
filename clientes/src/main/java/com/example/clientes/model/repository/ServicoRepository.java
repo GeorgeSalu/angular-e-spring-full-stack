bdd42babb86b3022c4a95950fc91e6670512b10d
GeorgeSalu <george.salu10@gmail.com>
2024-05-16 18:35:54 -0300

    projeto cliente - criando os repositorios jpa

---
+++ clientes/src/main/java/com/example/clientes/model/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package com.example.clientes.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.clientes.model.entity.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer>{
+
+}
