4aec599a5a783376df3d5532d5b72ca380a7c80e
GeorgeSalu <george.salu10@gmail.com>
2024-05-16 19:27:08 -0300

    projeto cliente - cadastro de cliente

--- clientes/src/main/java/com/example/clientes/rest/ClienteController.java
+++ clientes/src/main/java/com/example/clientes/rest/ClienteController.java
@@ -3,6 +3,7 @@ package com.example.clientes.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +23,8 @@ public class ClienteController {
 	}
 	
 	@PostMapping
-	@ResponseStatus(HttpStatus.OK)
-	public Cliente salvar(Cliente cliente) {
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar(@RequestBody Cliente cliente) {
 		return repository.save(cliente);
 	}
 	
