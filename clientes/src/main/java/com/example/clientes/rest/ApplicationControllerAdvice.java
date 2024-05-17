3b29db217fc1c5c22b236543a84f5b388cea5119
GeorgeSalu <george.salu10@gmail.com>
2024-05-17 20:53:21 -0300

    projeto cliente - padronizando o retorno de erro da api

--- clientes/src/main/java/com/example/clientes/rest/ApplicationControllerAdvice.java
+++ clientes/src/main/java/com/example/clientes/rest/ApplicationControllerAdvice.java
@@ -3,23 +3,27 @@ package com.example.clientes.rest;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.clientes.rest.exception.ApiErrors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public Object handleValidationErros(MethodArgumentNotValidException ex) {
 		BindingResult bindingResult = ex.getBindingResult();
 		List<String> messages = bindingResult.getAllErrors()
 							.stream()
 							.map( objectErro -> objectErro.getDefaultMessage() )
 							.collect(Collectors.toList());
-		
+		return new ApiErrors(messages);
 	}
 	
 }
