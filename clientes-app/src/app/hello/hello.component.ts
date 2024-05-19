b32f905bf301df12b48cc16e2d6601cb020f9760
GeorgeSalu <george.salu10@gmail.com>
2024-05-19 10:13:37 -0300

    cliente-app - templates e interpolacao

--- clientes-app/src/app/hello/hello.component.ts
+++ clientes-app/src/app/hello/hello.component.ts
@@ -7,9 +7,24 @@ import { Component } from "@angular/core";
 export class HelloComponent {
 
     nome: string;
+    clientes: Cliente[];
 
     constructor() {
-        this.nome = "george"
+        this.nome = "george";
+
+        let fulano = new Cliente('fulano', 22);
+        let cicrano = new Cliente('cicrano', 29);
+        let outro = new Cliente('outro', 22);
+
+        this.clientes = [fulano, cicrano, outro];
     }
 
+}
+
+class Cliente {
+
+    constructor(
+        public nome: string,
+        public idade: number
+    ) { }
 }
