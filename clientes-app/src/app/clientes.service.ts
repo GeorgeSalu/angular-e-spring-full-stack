e59de4ce65b00fb7d7ee65f4ceae5a370c54943d
GeorgeSalu <george.salu10@gmail.com>
2024-05-24 20:38:23 -0300

    clientes-app - listando os clientes atraves da api

--- clientes-app/src/app/clientes.service.ts
+++ clientes-app/src/app/clientes.service.ts
@@ -16,16 +16,8 @@ export class ClientesService {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
   }
 
-  getCliente(): Observable<Cliente[]> {
-    return null;
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
   }
 
-  getClientes(): Cliente[] {
-    let cliente = new Cliente();
-    cliente.id = 1;
-    cliente.nome = 'fulano';
-    cliente.dataCadastro = '18/11/2020';
-    cliente.cpf = '93828323232';
-    return [cliente];
-  }
 }
