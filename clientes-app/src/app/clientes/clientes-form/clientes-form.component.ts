90397695afbb0fe4dc3ccd76ff92abb2e17d3637
GeorgeSalu <george.salu10@gmail.com>
2024-05-24 19:49:50 -0300

    clientes-app - tratando erros vindos da api

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -25,6 +25,8 @@ export class ClientesFormComponent implements OnInit {
       .salvar(this.cliente)
       .subscribe(response => {
         this.success = true;
+      }, errorResponse => {
+        this.errors = errorResponse.error.errors;
       })
   }
 
