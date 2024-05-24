adcdf392a73c3f632feaec6a69541051b8323430
GeorgeSalu <george.salu10@gmail.com>
2024-05-24 20:32:49 -0300

    clientes-app - criando o componente de listagem de clientes

--- clientes-app/src/app/clientes.service.ts
+++ clientes-app/src/app/clientes.service.ts
@@ -16,10 +16,16 @@ export class ClientesService {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
   }
 
-  getCliente(): Cliente {
-    let cliente: Cliente = new Cliente();
-    cliente.nome = 'fulano de tal';
-    cliente.cpf = '8888888888888';
-    return cliente;
+  getCliente(): Observable<Cliente[]> {
+    return null;
+  }
+
+  getClientes(): Cliente[] {
+    let cliente = new Cliente();
+    cliente.id = 1;
+    cliente.nome = 'fulano';
+    cliente.dataCadastro = '18/11/2020';
+    cliente.cpf = '93828323232';
+    return [cliente];
   }
 }
