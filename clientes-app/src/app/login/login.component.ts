dcef0e3f8b4b9ca5d716ee68280b321e76ee91a3
GeorgeSalu <george.salu10@gmail.com>
2024-05-28 19:24:51 -0300

    clientes-app - melhorias no formulario de regristro de usuario

--- clientes-app/src/app/login/login.component.ts
+++ clientes-app/src/app/login/login.component.ts
@@ -43,7 +43,10 @@ export class LoginComponent {
       .salvar(usuario)
       .subscribe(response => {
         this.mensagemSucesso = "cadastro realizado com sucesso! efetue o login";
-
+        this.cadastrando = false;
+        this.username = '';
+        this.password = '';
+        this.errors = [];
       }, errorResponse => {
         this.errors = errorResponse.error.errors;
 
