bb22c5245d21298f0cbc8108762483d4a6b71bdc
GeorgeSalu <george.salu10@gmail.com>
2024-05-30 22:49:25 -0300

    projeto agenda-api - api de favoritar contato

--- agenda-api/src/main/java/com/example/agendaapi/api/ContatoController.java
+++ agenda-api/src/main/java/com/example/agendaapi/api/ContatoController.java
@@ -1,10 +1,12 @@
 package com.example.agendaapi.api;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,4 +44,15 @@ public class ContatoController {
 		return repository.findAll();
 	}
 	
+	@PatchMapping("{id}/favoritos")
+	public void favorite(@PathVariable Integer id,@RequestBody Boolean favorito) {
+		Optional<Contato> contato = repository.findById(id);
+		contato.ifPresent(c -> {
+			c.setFavorito(favorito);
+			repository.save(c);
+		});
+	}
+	
 }
+
+
