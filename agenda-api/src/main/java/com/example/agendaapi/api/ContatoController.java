0091c5de20d36e44a0f49935abdf61baf09a6e9d
GeorgeSalu <george.salu10@gmail.com>
2024-05-30 22:45:09 -0300

    projeto agenda-api - api de listagem de contatos

--- agenda-api/src/main/java/com/example/agendaapi/api/ContatoController.java
+++ agenda-api/src/main/java/com/example/agendaapi/api/ContatoController.java
@@ -1,7 +1,10 @@
 package com.example.agendaapi.api;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,6 +35,11 @@ public class ContatoController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Integer id) {
 		repository.deleteById(id);
+	}
+	
+	@GetMapping
+	public List<Contato> list() {
+		return repository.findAll();
 	}
 	
 }
