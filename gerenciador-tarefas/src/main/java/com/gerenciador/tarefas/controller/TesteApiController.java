3abad96f2ab0fc51f58b932b420b793f62db327f
GeorgeSalu <george.salu10@gmail.com>
2024-06-01 14:40:59 -0300

    projeto gerenciador-tarefas - criando um controlador simples de uma rota basica com paramentro

--- gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
+++ gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
@@ -1,6 +1,7 @@
 package com.gerenciador.tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class TesteApiController {
 	@GetMapping("/teste-api")
 	private String teste() {
 		return "sucesso";
+	}
+	
+	@GetMapping("/teste-bem-vindo")
+	public String testeBemVindo(@RequestParam(name = "nome") String nome) {
+		return "Ola "+nome+", seja bem vindo";
 	}
 	
 }
