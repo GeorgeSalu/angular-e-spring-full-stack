b269fbad99104d628cdc89b45d8c61724857e454
GeorgeSalu <george.salu10@gmail.com>
2024-06-02 18:24:01 -0300

    projeto gerenciador-tarefas - criando controller de usuario

---
+++ gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package com.gerenciador.tarefas.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gerenciador.tarefas.entity.Usuario;
+import com.gerenciador.tarefas.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@PostMapping
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
+		return new ResponseEntity<>(usuarioService.salvarUsuario(usuario), HttpStatus.CREATED);
+	}
+	
+}
