059a93b398500a11faf2cbb92b31e85e5587f5ec
GeorgeSalu <george.salu10@gmail.com>
2024-06-03 18:33:32 -0300

    projeto gerenciador-tarefas - criando filtro de rotas spring security

--- gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/repository/IUsuarioRepository.java
+++ gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/repository/IUsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.gerenciador.tarefas.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.gerenciador.tarefas.entity.Usuario;
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long>{
 
+	Optional<Usuario> findByUsername(String username);
+	
 }
---
+++ gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/service/AutenticacaoService.java
@@ -0,0 +1,50 @@
+package com.gerenciador.tarefas.service;
+
+import java.util.Date;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AutenticacaoService {
+
+	static public void addJwtToken(HttpServletResponse response,String username) {
+		
+		String jwtToken = Jwts.builder()
+				.setSubject(username)
+				.setExpiration(new Date(System.currentTimeMillis() + 3600000))
+				.signWith(SignatureAlgorithm.HS512, "signinKey")
+				.compact();
+		
+		response.addHeader("Authorization", "Bearer "+jwtToken);
+		response.addHeader("Access-Control-Expose-Headers", "Authorization");
+		
+	}
+	
+	static public Authentication obterAutenticacao(HttpServletRequest request) {
+		
+		String token = request.getHeader("Authorization");
+		
+		if(token != null) {
+			Claims user = Jwts.parser()
+					.setSigningKey("signinKey")
+					.parseClaimsJws(token.replace("Bearer ", ""))
+					.getBody();
+			
+			if(user != null) {
+				return new UsernamePasswordAuthenticationToken(user, null, null);
+			} else {
+				throw new RuntimeException("autenticacao falhou");
+			}
+			
+		}
+		return null;
+		
+	}
+	
+}
