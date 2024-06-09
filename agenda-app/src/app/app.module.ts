ae1a6f393ae5180295693d652d16e8d78a07db64
GeorgeSalu <george.salu10@gmail.com>
2024-06-09 11:17:54 -0300

    projeto - agenda-app - melhorias no layout

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar"
 import { MatInputModule } from "@angular/material/input"
+import { MatTabsModule } from "@angular/material/tabs"
+import { MatTableModule } from "@angular/material/table"
+import { MatIconModule } from "@angular/material/icon"
+import { MatCardModule } from "@angular/material/card"
 
 import { ContatoComponent } from './contato/contato.component';
 import { ContatoService } from './contato.service';
@@ -27,6 +31,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     HttpClientModule,
     ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
     BrowserAnimationsModule
   ],
   providers: [ContatoService],
